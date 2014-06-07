0ed03a188ebc27157db23d0f43433cc60a681291
Kang-min Liu <gugod@gugod.org>
2014-06-07 16:04:40 +0200

    introduce a env var G0CR_CONFIG_PATH to point to a JSON text file that should contains the configuration.

--- lib/G0cr/Config.pm
+++ lib/G0cr/Config.pm
@@ -1,15 +1,25 @@
 package G0cr::Config;
+use v5.14;
 use strict;
 use warnings;
+use Mojo::JSON qw(decode_json);
+
+BEGIN {
+    defined($ENV{G0CR_CONFIG_FILE}) or die "G0CR_CONFIG_FILE env var is undefined.";
+    -f $ENV{G0CR_CONFIG_FILE} or die "G0CR_CONFIG_FILE env var does not point to a file.";
+}
+
+sub CONFIG_FILE() {
+    $ENV{G0CR_CONFIG_FILE}
+}
 
 sub load {
-    return {
-        storage => "/tmp/g0cr",
-        elasticsearch_node => {
-            host => '127.0.0.1',
-            port => '9200'
-        }
-    }
+    state $config;
+    return $config if defined $config;
+    open my $fh, "<", CONFIG_FILE or die $!;
+    local $/ = undef;
+    my $content = <$fh>;
+    $config = decode_json( $content );
 }
 
 1;
